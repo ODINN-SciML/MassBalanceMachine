84a798d1e6ff432a24bd2b1d7e6c5b0828f49269
Julian Biesheuvel <j.p.biesheuvel@student.tudelft.nl>
2024-06-19 02:09:01 +0200

    Added custom loss function and notebook for Iceland

--- regions/iceland/mbm/scripts-model-training/model_methods.py
+++ regions/iceland/mbm/scripts-model-training/model_methods.py
@@ -1,6 +1,8 @@
 """
 This script contains functions training the XGBoost model.
 
+The custom loss function is by Kamilla Hauknes Sjursen
+
 @Author: Julian Biesheuvel
 Email: j.p.biesheuvel@student.tudelft.nl
 Date Created: 04/06/2024
@@ -9,6 +11,8 @@ Date Created: 04/06/2024
 import xgboost as xgb
 import numpy as np
 import sklearn
+
+from sklearn.utils.validation import check_is_fitted
 
 
 def train_xgb_model(X, y, idc_list, params, scorer='neg_mean_squared_error', return_train=True):
@@ -40,27 +44,141 @@ def train_xgb_model(X, y, idc_list, params, scorer='neg_mean_squared_error', ret
     return clf, fitted_model, cvl
 
 
-def get_prediction_per_season(X_train_all, y_train_all, splits_all, best_model, months=12):
+# Custom objective function scikit learn api with metadata, to be used with custom XGBRegressor class
+
+def custom_mse_metadata(y_true, y_pred, metadata):
+    """
+    Custom Mean Squared Error (MSE) objective function for evaluating monthly predictions with respect to
+    seasonally or annually aggregated observations.
+
+    For use in cases where predictions are done on a monthly timescale and need to be aggregated to be
+    compared with the true aggregated seasonal or annual value. Aggregations are performed according to a
+    unique ID provided by metadata. The function computes gradients and hessians
+    used in gradient boosting methods, specifically for use with the XGBoost library's custom objective
+    capabilities.
+
+    Parameters
+    ----------
+    y_true : numpy.ndarray
+        True (seasonally or annually aggregated) values for each instance. For a unique ID,
+        values are repeated n_months times across the group, e.g. the annual mass balance for a group
+        of 12 monthly predictions with the same unique ID is repeated 12 times. Before calculating the
+        loss, the mean over the n unique IDs is taken.
+
+    y_pred : numpy.ndarray
+        Predicted monthly values. These predictions will be aggregated according to the
+        unique ID before calculating the loss, e.g. 12 monthly predictions with the same unique ID is
+        aggregated for evaluation against the true annual value.
+
+    metadata : numpy.ndarray
+        An ND numpy array containing metadata for each monthly prediction. The first column is mandatory
+        and represents the ID of the aggregated group to which each instance belongs. Each group identified
+        by a unique ID will be aggregated together for the loss calculation. The following columns in the
+        metadata can include additional information for each instance that may be useful for tracking or further
+        processing but are not used in the loss calculation, e.g. number of months to be aggregated or the name
+        of the month.
+
+        ID (column 0): An integer that uniquely identifies the group which the instance belongs to.
+
+    Returns
+    -------
+    gradients : numpy.ndarray
+        The gradient of the loss with respect to the predictions y_pred. This array has the same shape
+        as y_pred.
+
+    hessians : numpy.ndarray
+        The second derivative (hessian) of the loss with respect to the predictions y_pred. For MSE loss,
+        the hessian is constant and thus this array is filled with ones, having the same shape as y_pred.
+    """
+
+    # Initialize empty arrays for gradient and hessian
+    gradients = np.zeros_like(y_pred)
+    hessians = np.ones_like(y_pred)  # Ones in case of mse
+
+    # Unique aggregation groups based on the aggregation ID
+    unique_ids = np.unique(metadata[:, 0])
+
+    # Loop over each unique ID to aggregate accordingly
+    for uid in unique_ids:
+        # Find indexes for the current aggregation group
+        indexes = metadata[:, 0] == uid
+
+        # Aggregate y_pred for the current group
+        y_pred_agg = np.sum(y_pred[indexes])
+
+        # True value is the same repeated value for the group, so we can use the mean
+        y_true_mean = np.mean(y_true[indexes])
+
+        # Compute gradients for the group based on the aggregated prediction
+        gradient = y_pred_agg - y_true_mean
+        gradients[indexes] = gradient
+
+    return gradients, hessians
+
+
+# Get true values (means) and predicted values (aggregates)
+
+def get_ytrue_y_pred_agg(y_true, y_pred, X):
+    # Extract the metadata
+    metadata = X[:, -3:]  # Assuming last three columns are the metadata
+    unique_ids = np.unique(metadata[:, 0])  # Assuming ID is the first column
+    y_pred_agg_all = []
+    y_true_mean_all = []
+
+    # Loop over each unique ID to calculate MSE
+    for uid in unique_ids:
+        # Indexes for the current ID
+        indexes = metadata[:, 0] == uid
+        # Aggregate y_pred for the current ID
+        y_pred_agg = np.sum(y_pred[indexes])
+        y_pred_agg_all.append(y_pred_agg)
+        # True value is the mean of true values for the group
+        y_true_mean = np.mean(y_true[indexes])
+        y_true_mean_all.append(y_true_mean)
+
+    y_pred_agg_all_arr = np.array(y_pred_agg_all)
+    y_true_mean_all_arr = np.array(y_true_mean_all)
+
+    return y_true_mean_all_arr, y_pred_agg_all_arr
+
+
+def get_aggregated_predictions(X, y, splits, model, season_month=12):
     y_pred_list = []
-    y_test_list = []
+    y_true_list = []
+    unique_ids_list = []
 
-    for train_index, test_index in splits_all:
-        # Loops over n_splits iterations and gets train and test splits in each fold
-        X_train, X_test = X_train_all[train_index], X_train_all[test_index]
-        y_train, y_test = y_train_all[train_index], y_train_all[test_index]
+    for train_index, test_index in splits:
+        # Split the data
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-        best_model.fit(X_train, y_train)
-        y_pred = best_model.predict(X_test)
+        # Fit the model
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
 
-        indices = np.argwhere((X_test == months))[:, 0]
-        y_test_crop = y_test[indices]
-        y_pred_crop = y_pred[indices]
+        # Filter by season
+        mask = X_test[:, -2] == season_month
+        X_filtered = X_test[mask]
+        y_true_filtered = y_test[mask]
+        y_pred_filtered = y_pred[mask]
 
-        y_test_list.extend(y_test_crop)
-        y_pred_list.extend(y_pred_crop)
+        # Extract metadata (assumed last three columns) and unique IDs
+        metadata = X_filtered[:, -3:]
+        unique_ids = np.unique(metadata[:, 0])
 
-    # Arrays of predictions and observations for each fold
-    y_test_season = np.hstack([*y_test_list])
-    y_pred_season = np.hstack([*y_pred_list])
+        for uid in unique_ids:
+            # Indexes for the current ID
+            indexes = metadata[:, 0] == uid
 
-    return y_test_season, y_pred_season
+            # Aggregate predictions and true values for the current ID
+            y_pred_agg = np.sum(y_pred_filtered[indexes])
+            y_true_mean = np.mean(y_true_filtered[indexes])
+
+            y_pred_list.append(y_pred_agg)
+            y_true_list.append(y_true_mean)
+            unique_ids_list.append(uid)
+
+    y_pred_agg_all = np.array(y_pred_list)
+    y_true_mean_all = np.array(y_true_list)
+
+    return y_true_mean_all, y_pred_agg_all
