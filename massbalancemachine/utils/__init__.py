d0757e83aa4a8f741dae2b547ae25ed87dc001e2
Julian Biesheuvel <j.p.biesheuvel@student.tudelft.nl>
2024-07-12 01:35:27 +0200

    Finished the other example notebook for custom stake data formats.

--- massbalancemachine/utils/__init__.py
+++ massbalancemachine/utils/__init__.py
@@ -1,3 +1,5 @@
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+import os, sys
 
-from pre_processing_data import *
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from pre_processing_data import convert_to_wgms, get_rgi, transform_crs
