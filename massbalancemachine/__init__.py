3a27247b7eb818029e5d7acf8c3f8a447c9f19d9
Julian Biesheuvel <j.p.biesheuvel@student.tudelft.nl>
2024-07-17 20:30:06 +0200

    Reimplemented (optimized) the custom loss function for XGBoost

--- massbalancemachine/__init__.py
+++ massbalancemachine/__init__.py
@@ -1,6 +1,7 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-__all__ = ['data_processing', 'utils']
+__all__ = ['xgboost_model', 'data_processing', 'utils']
 
+from .xgboost_model import *
 from .data_processing import *
 from .utils import *
