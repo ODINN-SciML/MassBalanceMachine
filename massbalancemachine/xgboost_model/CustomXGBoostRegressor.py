7e70221228a96158b8a5ac5e86ef6a8c1cda3270
Julian Biesheuvel <j.p.biesheuvel@student.tudelft.nl>
2024-07-17 20:49:48 +0200

    Refactored the code of the custom xgboost class

--- massbalancemachine/xgboost_model/CustomXGBoostRegressor.py
+++ massbalancemachine/xgboost_model/CustomXGBoostRegressor.py
@@ -1,5 +1,3 @@
-import numba
-
 import numpy as np
 import pandas as pd
 
@@ -7,36 +5,9 @@ from xgboost import XGBRegressor
 from sklearn.utils.validation import check_is_fitted
 
 
-def custom_mse_metadata(y_true, y_pred, metadata):
-    # Initialize gradients and hessians
-    gradients = np.zeros_like(y_pred)
-    hessians = np.ones_like(y_pred)
-
-    # Create a DataFrame from metadata
-    df_metadata = pd.DataFrame(metadata, columns=['ID', 'N_MONTHS', 'MONTH'])
-
-    # Aggregate y_pred and y_true for each group
-    grouped_ids = df_metadata.assign(y_true=y_true, y_pred=y_pred).groupby('ID')
-    y_pred_agg = grouped_ids['y_pred'].sum().values
-    y_true_mean = grouped_ids['y_true'].mean().values
-
-    # Compute gradients
-    gradients_agg = y_pred_agg - y_true_mean
-
-    # Create a mapping from ID to gradient
-    gradient_map = dict(zip(grouped_ids.groups.keys(), gradients_agg))
-
-    # Assign gradients to corresponding indices
-    df_metadata['gradient'] = df_metadata['ID'].map(gradient_map)
-    gradients[df_metadata.index] = df_metadata['gradient'].values
-
-    return gradients, hessians
-
-
 class CustomXGBoostRegressor(XGBRegressor):
     def __init__(self, **kwargs):
         super(CustomXGBoostRegressor, self).__init__(**kwargs)
-
 
     def fit(self, X, y, **fit_params):
         # Split features from metadata
@@ -53,7 +24,7 @@ class CustomXGBoostRegressor(XGBRegressor):
 
         # Define closure that captures metadata for use in custom objective
         def custom_objective(y_true, y_pred):
-            return custom_mse_metadata(y_true, y_pred, metadata)
+            return self._custom_mse_metadata(y_true, y_pred, metadata)
 
         # Set custom objective
         self.set_params(objective=custom_objective)
@@ -77,12 +48,7 @@ class CustomXGBoostRegressor(XGBRegressor):
 
         y_pred = self.predict(features)
 
-        df_metadata = pd.DataFrame(metadata, columns=['ID', 'N_MONTHS', 'MONTH'])
-
-        # Aggregate y_pred and y_true for each group
-        grouped_ids = df_metadata.assign(y_true=y, y_pred=y_pred).groupby('ID')
-        y_pred_agg = grouped_ids['y_pred'].sum().values
-        y_true_mean = grouped_ids['y_true'].mean().values
+        y_pred_agg, y_true_mean, _, _ = self._create_metadata_scores(metadata, y, y_pred)
 
         # Calculate MSE
         mse = ((y_pred_agg - y_true_mean) ** 2).mean()
@@ -94,3 +60,34 @@ class CustomXGBoostRegressor(XGBRegressor):
         check_is_fitted(self)
 
         return super().predict(features)
+
+    @staticmethod
+    def _custom_mse_metadata(y_true, y_pred, metadata):
+        # Initialize gradients and hessians
+        gradients = np.zeros_like(y_pred)
+        hessians = np.ones_like(y_pred)
+
+        y_pred_agg, y_true_mean, grouped_ids, df_metadata = CustomXGBoostRegressor._create_metadata_scores(metadata, y_true, y_pred)
+
+        # Compute gradients
+        gradients_agg = y_pred_agg - y_true_mean
+
+        # Create a mapping from ID to gradient
+        gradient_map = dict(zip(grouped_ids.groups.keys(), gradients_agg))
+
+        # Assign gradients to corresponding indices
+        df_metadata['gradient'] = df_metadata['ID'].map(gradient_map)
+        gradients[df_metadata.index] = df_metadata['gradient'].values
+
+        return gradients, hessians
+
+    @staticmethod
+    def _create_metadata_scores(metadata, y1, y2):
+        df_metadata = pd.DataFrame(metadata, columns=['ID', 'N_MONTHS', 'MONTH'])
+
+        # Aggregate y_pred and y_true for each group
+        grouped_ids = df_metadata.assign(y_true=y1, y_pred=y2).groupby('ID')
+        y_pred_agg = grouped_ids['y_pred'].sum().values
+        y_true_mean = grouped_ids['y_true'].mean().values
+
+        return y_pred_agg, y_true_mean, grouped_ids, df_metadata
