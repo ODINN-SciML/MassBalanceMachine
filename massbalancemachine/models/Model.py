efeec5558ddeb7ed38682b7094c9528b9fcabb26
Julian Biesheuvel <j.p.biesheuvel@student.tudelft.nl>
2024-08-07 13:02:05 +0200

    Downgraded the Numpy version Git issue #46

--- massbalancemachine/models/Model.py
+++ massbalancemachine/models/Model.py
@@ -1,33 +1,21 @@
-from abc import ABC, abstractmethod
 from massbalancemachine.dataloader import DataLoader
 
 
-class Model(ABC):
-    def __init__(self): #**params
-        pass
-        # # TODO
-        # default_params = {
-        #     ...
-        # }
-        # default_params.update(params)
-        # self.params = default_params
-
-    @abstractmethod
-    def fit(self, dataloader: DataLoader, loss='reg:squarederror'):
+class Model():
+    def __init__(self):
         pass
 
-    @abstractmethod
+    def fitt(self, dataloader: DataLoader, loss='reg:squarederror'):
+        pass
+
     def predict(self, dataloader: DataLoader):
         pass
 
-    @abstractmethod
     def perform_gridsearch(self, dataloader: DataLoader, random=True, loss='reg:squarederror', score='reg:squarederror', **params):
         pass
 
-    @abstractmethod
     def monthly_loss(self, metric='MSE'):
         pass
 
-    @abstractmethod
     def score(self):
         pass
