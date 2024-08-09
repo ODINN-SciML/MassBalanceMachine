4cb97cad804a8af176de6b91928cb035871acd99
Julian Biesheuvel <j.p.biesheuvel@student.tudelft.nl>
2024-08-09 20:34:48 +0200

    Continued working on the XGBoost Model example notebook

--- massbalancemachine/__init__.py
+++ massbalancemachine/__init__.py
@@ -1,8 +1,7 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-__all__ = ['dataloader', 'models', 'data_processing', 'utils'] #
+__all__ = ['dataloader', 'models', 'data_processing']
 
 from .dataloader import *
 from .models import *
 from .data_processing import *
-from .utils import *
