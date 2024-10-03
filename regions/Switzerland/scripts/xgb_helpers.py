4f2fd2ad24c6dd440e4af8cfde3de31ab45fef0a
marvande <vmarijn@ethz.ch>
2024-10-03 16:04:45 +0000

    update multi gl

--- regions/Switzerland/scripts/xgb_helpers.py
+++ regions/Switzerland/scripts/xgb_helpers.py
@@ -143,3 +143,20 @@ def plot_gsearch_results(grid, params):
 
     plt.legend()
     plt.show()
+    
+    
+def getDfAggregatePred(test_set, y_pred_agg, all_columns):    
+    # Aggregate predictions to annual or winter:
+    df_pred = test_set['df_X'][all_columns].copy()
+    df_pred['target'] = test_set['y']
+    grouped_ids = df_pred.groupby('ID').agg({
+        'target': 'mean',
+        'YEAR': 'first',
+        'POINT_ID': 'first'
+    })
+    grouped_ids['pred'] = y_pred_agg
+    grouped_ids['PERIOD'] = test_set['df_X'][all_columns].groupby('ID')['PERIOD'].first()
+    grouped_ids['GLACIER'] = grouped_ids['POINT_ID'].apply(
+        lambda x: x.split('_')[0])
+    
+    return grouped_ids
