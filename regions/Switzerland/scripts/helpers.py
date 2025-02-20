a2f3b8f7dba48b578f23267c8045730227f4cd44
marvande <vmarijn@ethz.ch>
2025-02-20 10:46:59 +0000

    fixes in PR for CH

--- regions/Switzerland/scripts/helpers.py
+++ regions/Switzerland/scripts/helpers.py
@@ -4,6 +4,7 @@ import numpy as np
 import random as rd
 import torch
 import gc
+import shutil
 
 from matplotlib import pyplot as plt
 from matplotlib.colors import to_hex
@@ -26,8 +27,8 @@ path_SMB_GLAMOS_csv = '../../../data/GLAMOS/glacier-wide/csv/'
 # Gridded data for MBM to use for making predictions over whole grid (SGI or RGI grid)
 path_glacier_grid_rgi = '../../../data/GLAMOS/topo/gridded_topo_inputs/RGI_grid/' # DEMs & topo
 path_glacier_grid_sgi = '../../../data/GLAMOS/topo/gridded_topo_inputs/SGI_grid/' # DEMs & topo 
-path_glacier_grid_glamos = '../../../data/GLAMOS/topo/gridded_topo_inputs/GLAMOS_grid/'
-
+path_glacier_grid_glamos = '../../../data/GLAMOS/topo/gridded_topo_inputs/GLAMOS_grid_with_oggm_vars/'
+# path_glacier_grid_glamos = '../../../data/GLAMOS/topo/gridded_topo_inputs/GLAMOS_grid/'
 
 # Topo data
 path_SGI_topo = '../../../data/GLAMOS/topo/SGI2020/' # DEMs & topo from SGI
@@ -107,18 +108,20 @@ def seed_all(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+
 def emptyfolder(path):
     if os.path.exists(path):
-        # Loop through all items in the directory
         for item in os.listdir(path):
             item_path = join(path, item)
-            if isfile(item_path):
-                os.remove(item_path)  # Remove file
-            elif isdir(item_path):
-                emptyfolder(item_path)  # Recursively empty the folder
-                os.rmdir(item_path)  # Remove the now-empty folder
+            try:
+                if isfile(item_path):
+                    os.remove(item_path)  # Remove file
+                elif isdir(item_path):
+                    shutil.rmtree(item_path)  # Remove folder and all its contents
+            except Exception as e:
+                print(f"Error removing {item_path}: {e}")
     else:
-        createPath(path)
+        os.makedirs(path, exist_ok=True)  # Ensures the path exists
 
 
 def createPath(path):
