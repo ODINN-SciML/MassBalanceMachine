d7c11f9de5e93db5e30b6bda9f091a9b7ebc8b64
marvande <vmarijn@ethz.ch>
2026-06-04 13:24:56 +0000

    fix imports

--- massbalancemachine/plots/__init__.py
+++ massbalancemachine/plots/__init__.py
@@ -1,4 +1,10 @@
-from plots.perf_plots import predVSTruth, predVSTruthPerGlacier, predVSTruthGlacierWide
+from plots.perf_plots import (
+    predVSTruth,
+    predVSTruthPerGlacier,
+    predVSTruthGlacierWide,
+    plotMeanPred,
+    predVSTruthTimeSeries,
+)
 from plots.style import use_mbm_style, COLOR_ANNUAL, COLOR_WINTER
 from plots.input_plot import histogram_mb, scatterplot_mb
 from plots.map_plots import mapGlacier
