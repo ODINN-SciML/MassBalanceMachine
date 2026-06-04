09d849b8fb63665ac413a335c9fb105301920d83
marvande <vmarijn@ethz.ch>
2026-06-04 13:26:11 +0000

    fix imports

--- massbalancemachine/plots/__init__.py
+++ massbalancemachine/plots/__init__.py
@@ -10,3 +10,5 @@ from plots.input_plot import histogram_mb, scatterplot_mb
 from plots.map_plots import mapGlacier
 from plots.train_plots import plot_training_history
 from plots.style import use_mbm_style, COLOR_ANNUAL, COLOR_WINTER
+from plots.profile_plots import profilePerGlacier
+from plots.temporal_plots import cumulatedMassChange
